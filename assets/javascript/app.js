7499fc20a4766ba5e1d6de9e5b590f152d5a5ed1
Ryan Kooy <rwkooy@gmail.com>
2019-08-05 21:59:26 -0400

    gifs are propogating

---
+++ assets/javascript/app.js
@@ -0,0 +1,79 @@
+// create buttons from an array
+// pushing new buttons into the array using user input values
+// show gif ratings
+// start/stop gifs with click
+// limit image # to 10
+
+$(document).ready(function() {
+
+    bArray = ['bats', 'cats', 'gnats', 'hats', 'mats', 'pats', 'rats', 'tats', 'vats'];
+
+    var b = '';
+    var thing = '';
+
+    function makeButtons() {
+
+        for (var i = 0; i < bArray.length; i++) {
+
+            b = $('<button>');
+            b.addClass('oldB');
+            b.attr(bArray[i]);
+            b.text(bArray[i]);
+            $('.buttons').append(b);
+
+        }
+
+    }
+
+    $('.buttons').on('click', '.oldB', function() {
+
+        $('#gifs').empty();
+
+        thing = $(this).text();
+
+        console.log(thing);
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + thing + "&api_key=8QgkvoLZ85mVtpm3jbBqQIEXBObzEPnh&limit=10";
+
+        $.ajax({
+
+            url: queryURL,
+            method: 'GET'
+
+        }).then(function(resp) {
+
+            console.log(resp);
+
+            var r = resp.data;
+
+            for (var j = 0; j < r.length; j++) {
+
+                var gif = $('<div>');
+
+                var rating = r[j].rating;
+
+                var p = $('<p>').text("Rating: " + rating);
+
+                var pic = $('<img>');
+
+                pic.attr('src', r[j].images.fixed_height.url);
+                pic.addClass('pic');
+
+                gif.append(p);
+                gif.append(pic);
+
+                $('#gifs').prepend(gif);
+
+            }
+
+
+            
+        });
+
+
+
+    });
+
+    makeButtons();
+    
+});
