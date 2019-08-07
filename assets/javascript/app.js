b31191becea0dfa074804337f4dcaafc29cb9861
Ryan Kooy <rwkooy@gmail.com>
2019-08-07 17:55:56 -0400

    gifs pausing nicely

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,8 +31,6 @@ $(document).ready(function() {
 
         thing = $(this).text();
 
-        console.log(thing);
-
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + thing + "&api_key=8QgkvoLZ85mVtpm3jbBqQIEXBObzEPnh&limit=10";
 
         $.ajax({
@@ -56,13 +54,27 @@ $(document).ready(function() {
 
                     var pic = $('<img>');
 
-                    pic.attr('src', r[j].images.fixed_height.url);
+                    pic.attr('src', r[j].images['480w_still'].url);
+                    pic.attr('data-still', r[j].images['480w_still'].url);
+                    pic.attr('data-animate', r[j].images.fixed_height.url);
+                    pic.attr('data-state', 'still');
                     pic.addClass('pic');
 
                     gif.append(p);
                     gif.append(pic);
 
                     $('#gifs').prepend(gif);
+
+                    $('.pic').on('click', function() {
+                        var s = $(this).attr('data-state');
+                        if (s === 'still') {
+                            $(this).attr('src', $(this).attr('data-animate'));
+                            $(this).attr('data-state', 'animate');
+                        } else {
+                            $(this).attr('src', $(this).attr('data-still'));
+                            $(this).attr('data-state', 'still');
+                        }
+                    });
 
                 }
 
